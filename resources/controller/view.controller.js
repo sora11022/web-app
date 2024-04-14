15a2356418079c7e8db6c9a4415be4913ad86279
sora11022 <tuongdc.odin@gmail.com>
2024-04-15 00:25:18 +0700

    s

--- resources/controller/view.controller.js
+++
@@ -1,8 +0,0 @@
-const accountModel = require('../models/account.model');
-
-module.exports = {
-  renderChat: async (req, res) => {
-    const accounts = await accountModel.find();
-    return res.render('chat.ejs', { accounts: accounts });
-  },
-};
--- resources/routers/index.js
+++
@@ -1,7 +0,0 @@
-const accountRouter = require('./account.router');
-const viewRouter = require('./view.router');
-
-module.exports = (app) => {
-  app.use('/api/accounts', accountRouter);
-  app.use('/', viewRouter);
-};
